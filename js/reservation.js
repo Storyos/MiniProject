b41df89b8635cbfb9acf2004493d2d10dcfc9135
Storyos <storyoser@pukyong.ac.kr>
2024-01-29 12:14:10 +0900

    KBG_영화선택 리스트 추가

---
+++ js/reservation.js
@@ -0,0 +1,8 @@
+
+const movie_menu = document.querySelector("#reserve-movie");
+const theater_menu = document.querySelector("#reserve-theater");
+const date_menu = document.querySelector("#reserve-datentime")
+
+date_menu.addEventListener("click", (e) =>{
+    e.preventDefault();
+})
