fa3dddbad2f86b31703e5809257b272d6a46dcdb
Storyos <storyoser@pukyong.ac.kr>
2024-01-29 12:28:36 +0900

    KBG_예약페이지 js 로직 추가

--- js/reservation.js
+++ js/reservation.js
@@ -2,7 +2,11 @@
 const movie_menu = document.querySelector("#reserve-movie");
 const theater_menu = document.querySelector("#reserve-theater");
 const date_menu = document.querySelector("#reserve-datentime")
+const movie_list = document.querySelector("#movie-list")
+const movie_title = ['웡카','시민덕희','소풍','도그데이즈','외계+인 2부','위시','서울의 봄','데드맨'];
+const movie_time = ['116분','113분','113분','120분','122분','95분','141분','108분'];
+const movie_date = ['2024.01.31]','2024.01.24','2024.02.07','2024.02.07','2024.01.10','2024.01.03','2023.11.22','2024.02.07'];
 
-date_menu.addEventListener("click", (e) =>{
-    e.preventDefault();
-})
+for(let i = 0; i <8; i++){
+
+}
