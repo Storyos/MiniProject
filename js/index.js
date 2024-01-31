57b561c129a31596e23bdc34d3abb8f28e6a44dd
DFItss <edian113@naver.com>
2024-01-31 17:51:09 +0900

    lss_index파일 수정 및 오류수정

--- js/index.js
+++ js/index.js
@@ -5,7 +5,6 @@ const topMenu_right = document.getElementById("header_bot_grid03")
 const topMenu_center_btn = document.querySelectorAll(".header_menu_btn a")
 const topMenu_right_btn = document.querySelectorAll("#header_bot_grid03 a")
 const topDropMenu = document.getElementById("header_bot_drop")
-console.log(topDropMenu)
 
 let topMenuY = topMenu.offsetTop;
 
@@ -40,29 +39,49 @@ window.addEventListener('scroll', () => {
 // 메뉴 마우스오버 > 드롭다운 메뉴 생성
 const dropDown = document.querySelectorAll(".header_menu_btn")
 const dropDownMenu = document.querySelectorAll(".dropBox")
-console.log(dropDown)
-console.log(dropDownMenu)
+const dropDownBox = document.getElementById('header_bot_drop')
+const menuBox = document.getElementById('header_top')
+console.log(dropDownBox)
 
 dropDown.forEach((e)=>{
   e.addEventListener('mouseover', ()=>{
     if(e===dropDown[0]){
-      console.log('예매')
+      // console.log('예매')
+      dropDownMenu.forEach(e => {
+        e.style.display='none'
+      });
       dropDownMenu[0].style.display = ""
     }else if(e===dropDown[1]){
-      console.log('영화')
+      // console.log('영화')
+      dropDownMenu.forEach(e => {
+        e.style.display='none'
+      });
       dropDownMenu[1].style.display = ""
     }else if(e===dropDown[2]){
-      console.log('영화관')
+      // console.log('영화관')
+      dropDownMenu.forEach(e => {
+        e.style.display='none'
+      });
       dropDownMenu[2].style.display = ""
     }else if(e===dropDown[3]){
-      console.log('이벤트')
+      // console.log('이벤트')
+      dropDownMenu.forEach(e => {
+        e.style.display='none'
+      });
       dropDownMenu[3].style.display = ""
     }else if(e===dropDown[4]){
-      console.log('스토어')
+      // console.log('스토어')
+      dropDownMenu.forEach(e => {
+        e.style.display='none'
+      });
       dropDownMenu[4].style.display = ""
     }
-
-    e.addEventListener('mouseout', ()=>{
+    menuBox.addEventListener('mouseover', ()=>{
+      dropDownMenu.forEach(e => {
+        e.style.display='none'
+      });
+    })
+    dropDownBox.addEventListener('mouseleave', ()=>{
       dropDownMenu.forEach(e => {
         e.style.display='none'
       });
@@ -71,5 +90,71 @@ dropDown.forEach((e)=>{
 })
 
 // 모달박스버튼
-const modarIcon = document.getElementById('header_bot_grid03')
-console.log(modarIcon)
+const modarIcon = document.querySelectorAll('#header_bot_grid03 a')
+const modarBox = document.getElementById('index_modal')
+const modarBoxEsc = document.getElementById('index_modal_esc')
+
+modarIcon[2].addEventListener('click', (e)=>{
+  modarBox.style.display = ""
+  modarBox.style.boxShadow = 'rgba(0,0,0,0.5) 0 0 0 9999px';
+  document.body.style.cssText = `
+    position: fixed; 
+    top: -${window.scrollY}px;
+    overflow-y: scroll;
+    width: 100%;`;
+})
+modarBoxEsc.addEventListener('click', (e)=>{
+  modarBox.style.display = "none"
+  document.body.style.cssText = ``;
+  modarBox.style.boxShadow = '';
+  // window.scrollTo(0,window.scrollY);
+})
+
+
+//ImgSlide
+let slideIndex = 0;
+const slideImg = document.querySelectorAll('.content_slides_box li');
+const slideBox = document.querySelector('.content_slides_box');
+function moveImgSlide(n) {
+  slideIndex += n;
+  if (slideIndex >= slideImg.length) {
+    slideIndex = 0; // 끝에 도달하면 처음 이미지부터 다시 시작
+  }
+  if (slideIndex < 0) {
+    slideIndex = slideImg.length - 1;
+  }
+  const offset = -slideIndex * 25; // 슬라이드 이동 거리 계산 (단위: %)
+  slideBox.style.transform = `translateX(${offset}%)`;
+}
+
+// cardSlide
+let cardIndex = 0;
+const cards = document.querySelectorAll('#content_cardBox li')
+const cardBox = document.getElementById('content_cardBox')
+const lArrow = document.querySelector('.card_prev')
+const rArrow = document.querySelector('.card_next')
+
+function moveCardSlide(e){
+  const cardOffset = -e * 9.1; // 슬라이드 이동 거리 계산 (단위: %)
+  cardBox.style.transform = `translateX(${cardOffset}%)`
+}
+
+lArrow.addEventListener('click', ()=>{
+  cardIndex--;
+  if(cardIndex<0){
+    cardIndex = cards.length-5
+  }
+  
+  console.log(cardIndex)
+  moveCardSlide(cardIndex)
+})
+rArrow.addEventListener('click', ()=>{
+  cardIndex++;
+  if(cardIndex>cards.length-5){
+    cardIndex = 0;
+  }
+  console.log(cardIndex)
+  moveCardSlide(cardIndex)
+})
+
+
